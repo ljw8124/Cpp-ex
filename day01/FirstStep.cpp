27cddd49806fb01964aaf741aefb6956b2754626
Joungwoo Lee <ljw8124@gmail.com>
2023-08-16 12:18:27 +0900

    C++ 기본

---
+++ day01/FirstStep.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 이정우 on 2023/08/16.
+//
+
+#include "FirstStep.h"
---
+++ day01/FirstStep.h
@@ -0,0 +1,18 @@
+//
+// Created by 이정우 on 2023/08/16.
+//
+//
+//#ifndef C___EX_FIRSTSTEP_H
+//#define C___EX_FIRSTSTEP_H
+
+#include <iostream>
+
+int main()
+{
+    // 표준 출력 스트림으로 문장을 출력함
+    std::cout << "나의 첫 번째 C++ 프로그램"
+              << std::endl;
+    return 0;
+}
+
+//#endif //C___EX_FIRSTSTEP_H
