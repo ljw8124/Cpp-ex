a789d3f0e5de42e2b2101081c39cc0bc229bb090
Joungwoo Lee <ljw8124@gmail.com>
2023-08-16 12:22:19 +0900

    cpp 파일로 소스 이동

--- day01/FirstStep.cpp
+++ day01/FirstStep.cpp
@@ -1,5 +1,21 @@
-//
-// Created by 이정우 on 2023/08/16.
-//
+// 해쉬문자 include ~ 식의 표현은 선행처리기의 표현 중 하나이다.
+#include <iostream>
 
-#include "FirstStep.h"
+int main()
+{
+    // 표준 출력 스트림으로 문장을 출력함
+    std::cout << "나의 첫 번째 C++ 프로그램"
+              << std::endl;
+    return 0;
+}
+
+void commentFunc(int a, int b)
+{
+    // a와 b의 값 중에서
+    // 더큰 값을 출력한다.
+    if(a > b)
+        std::cout << a << std::endl;
+    else
+        std::cout << b << std::endl;
+
+}
