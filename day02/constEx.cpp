b7d2d73ad34a951ac0feb59d640ff7c2aaf8a23b
Joungwoo Lee <ljw8124@gmail.com>
2023-08-17 12:57:59 +0900

    cpp day02

---
+++ day02/constEx.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 이정우 on 2023/08/17.
+//
+
+#include <iostream>
+
+int a;
+std:: cin >> a;
+
+const int b = 20;
+const int C1 = a;           // cin 을 통해 입력된 a 의 값으로 초기화
+
+constexpr int C2 = a + 10;  // 오류: 컴파일 시에 a 의 값을 입력받지 않았으므로 알 수가 없음 -> 따라서 컴파일 불가
+constexpr int C3 = b + 100; // b + 100 은 컴파일 시에 계산할 수가 있으므로 오류가 아님 -> 실행시간 조금이라도 단축 가능
+constexpr int C4 = C1 * 2;  // 컴파일 시에 C1 의 값을 알 수가 없음
+
+constexpr int fac(int n) {
+    return n > 1 ? n * fac(n-1) : 1;
+}
+
+void f(int x) {
+    constexpr int a = fac(4);       // 컴파일할 때 미리 계산
+    int b = fac(x);                 // 실행 중 계산
+}
---
+++ day02/scope2.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 이정우 on 2023/08/17.
+//
+
+// scope1.cpp 에서 가져오는것
+// extern 은 다른곳에서 존재한다는 것을 표시
+extern int a;
+
+// static 으로 선언했으므로 extren 불가능
+static int b{50};
+
+int g() {
+
+}
+
